e570ce39e767b9799e0d3a7d7487468f6c2015f2
Elfana, A | Elfaael <elfaael12@gmail.com>
2024-07-25 08:51:40 +0700

    Initial Commit

---
+++ eKoins/eKoinsApp.swift
@@ -0,0 +1,17 @@
+//
+//  eKoinsApp.swift
+//  eKoins
+//
+//  Created by Elfana Anamta Chatya on 25/07/24.
+//
+
+import SwiftUI
+
+@main
+struct eKoinsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
